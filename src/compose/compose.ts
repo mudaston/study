8eb77d1373d4ed9ef5a575dd0f15a08ef26bfa9b
mudaston <twil.revaas@gmail.com>
2023-05-09 18:54:45 +0300

    delete uneccessary files

--- src/compose/compose.ts
+++
@@ -1,8 +0,0 @@
-import type { Fn } from './types'
-
-const compose =
-	<T>(...args: Fn<T>[]) =>
-	(value: T) =>
-		args.reduceRight((result, func) => func(result), value)
-
-export { compose }
--- src/linked-list/types.ts
+++
@@ -1,4 +0,0 @@
-import { List } from './linked-list'
-
-
-export type Predicate<T> = (value: T, index: number, list: List<T>) => boolean
