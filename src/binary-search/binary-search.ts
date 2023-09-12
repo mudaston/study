9b8efd0ba70d44cd12a2b6e7428062475a8cb52d
ren.vasco <ren.vasco@kiv.dev>
2023-09-12 10:52:53 +0300

    Revert "Merge branch 'main' of https://github.com/mudaston/study into next"
    This reverts commit c020fa76868ba25606a4762c56ddccceb47c20df, reversing
    changes made to 9b3e5fcca16af48a1909d1ae02a055226b0f68a7.

--- src/binary-search/binary-search.ts
+++ src/binary-search/binary-search.ts
@@ -1,4 +1,4 @@
-const binarySEARCHMAIN = <T>(array: T[], key: T): number => {
+const binarySEARCHNEXT = <T>(array: T[], key: T): number => {
 	let low = 0
 	let high = array.length
 
@@ -24,4 +24,4 @@ const binarySEARCHMAIN = <T>(array: T[], key: T): number => {
 	return -1
 }
 
-export { binarySEARCHMAIN as binarySEARCH }
+export { binarySEARCHNEXT as binarySEARCH }
